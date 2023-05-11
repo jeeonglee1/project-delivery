77a8cdc206ff275470fe98096b05d566a52dd6cc
Samuel Choi <sam99choi@gmail.com>
2023-05-11 17:00:28 -0300

    change-tailwind-config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    color: {
+      '--white': '#FFF',
+      '--black': '#1A1A1A',
+      '--red': '#EA1D2C',
+      '--gray-100': '#A6A6A6',
+      '--gray-200': '#717171',
+      '--gray-400': '#3E3E3E',
+      '--gray-h2': '#3F3E3E',
+      '--green': '#50A773',
+    },
+
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
