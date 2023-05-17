42d78dce45f32d76da6030f1aa3cad2e6037290f
jeeonglee1 <jeeonglee1@gmail.com>
2023-05-16 22:01:56 -0300

    finish ListaPratos

--- src/components/ListaPratos/ListaPratos.tsx
+++ src/components/ListaPratos/ListaPratos.tsx
@@ -38,6 +38,28 @@ export default function ListaPratos() {
           <p className="flex justify-center items-center p-2 font-medium">Risoto Oriental</p>
         </div>
 
+        <div className="flex flex-col rounded-xl hover:-translate-y-0.5 transition">
+          <Image
+            className="rounded-lg w-[220px] h-[147px] shadow-cinza-200 shadow-md"
+            src="/../public/images/sanduiche2.avif"
+            alt="risoto oriental"
+            width={220}
+            height={100}
+          />
+          <p className="flex justify-center items-center p-2 font-medium">Sanduiche</p>
+        </div>
+
+        <div className="flex flex-col rounded-xl hover:-translate-y-0.5 transition">
+          <Image
+            className="rounded-lg w-[220px] h-[147px] shadow-cinza-200 shadow-md"
+            src="/../public/images/espetinho.avif"
+            alt="risoto oriental"
+            width={220}
+            height={100}
+          />
+          <p className="flex justify-center items-center p-2 font-medium">Espetinho</p>
+        </div>
+
       </div>
     </div>
   );
